8728c2f1d943e332561139ac8bc25b5f0e0612ba
Muneeb Mustafa <muneeb.mustafa080@gmail.com>
2024-09-21 15:02:42 +0500

    Portfolio Website

---
+++ Js/main.js
@@ -0,0 +1,52 @@
+// Header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+// nav hide
+
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+
+
+// script.js
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
